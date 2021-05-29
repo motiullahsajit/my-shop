432e90157e72c225d42d5e869d260e465c314da7
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-30 00:27:10 +0600

    set up redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,19 @@
+import { ActionsTypes } from "../contants/action-types";
+
+const initialState = {
+  products: [{
+    id: 1,
+    title: "Dipesh",
+    category: "programmer",
+  },
+  ],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionsTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
