4a7f6115fc9ba70847791ef819a22b4a11482de9
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-30 00:30:41 +0600

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='ui fixed menu'>
+      <div className="ui container center">
+        <h3>My Shop</h3>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
